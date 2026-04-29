4db1e38987d0e7491734de93af6c020623624430
anshita647 <dasanshita2004@gmail.com>
2026-04-30 00:32:10 +0530

    add backend vercel config

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,4 @@
-import app from "../server/app.js"; // your express app
+import serverless from "serverless-http";
+import app from "../server.js";
 
-export default function handler(req, res) {
-  return app(req, res);
-}
+export default serverless(app);
--- server/server.js
+++ server/server.js
@@ -1,39 +1,33 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-dotenv.config();  
+dotenv.config();
+
 import connectDB from "./configs/db.js";
 import userRouter from "./routes/userRoutes.js";
 import resumeRouter from "./routes/resumeRoutes.js";
 import aiRouter from "./routes/aiRoutes.js";
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+// connect DB
+connectDB();
 
 app.use(express.json());
+
 app.use(cors({
   origin: "https://frontened-mu.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
-app.options(/.*/, cors());
+
 app.get("/", (req, res) => {
   res.send("Server is live...");
 });
-app.use('/api/users',userRouter)
-app.use('/api/resumes',resumeRouter)
-app.use('/api/ai', aiRouter);
-// Start server only after DB connects
-const startServer = async () => {
-  try {
-    await connectDB();
-    console.log("✅ Database connected");
 
-    
+// Routes
+app.use("/api/users", userRouter);
+app.use("/api/resumes", resumeRouter);
+app.use("/api/ai", aiRouter);
 
-  } catch (error) {
-    console.error("❌ Failed to start server:", error.message);
-  }
-};
-
-startServer();
 export default app;
