34a51cae28b1092380c2ef400d8f8fcdaec7c84c
anshita647 <dasanshita2004@gmail.com>
2026-06-18 14:17:01 +0530

    Update CORS origin for frontend application

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://frontened-mu.vercel.app",
+  origin: "https://resume-builder-frontened.onrender.com",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
